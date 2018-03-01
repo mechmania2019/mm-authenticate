888d6f9ca0df57fa4b98392d82a88d88280b2d75
Pranay Prakash <pranay.gp@gmail.com>
2018-03-01 02:11:02 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const { send } = require('micro')
+
+const authenticate = mongoose => {
+  const { Team } = require('mm-schemas')(mongoose)
+  return fn => async (req, res) => {
+    const header = req.headers.authorization
+    if(!header) {
+      return send(res, 401, 'Unauthorized')
+    }
+    const [type, token] = header.split(' ')
+    switch(type) {
+      case 'Bearer':
+        const team = await Team.findOne({token}).exec()
+        if(!team) {
+          return send(res, 401, 'Unauthorized')
+        }
+        req.user = team
+        return await fn(req, res)
+      default:
+        return send(res, 401, 'Unauthorized')
+    }
+  }
+}
+
+module.exports = authenticate
